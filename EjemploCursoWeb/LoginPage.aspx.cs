8e3848713e024d909673c0ba7749d730fa08fd7c
agil <no-reply@no-email.com>
2024-07-10 14:41:51 +0200

    Configuramos la conexión desde la página de login con el servicio WCF

--- EjemploCursoWeb/LoginPage.aspx.cs
+++ EjemploCursoWeb/LoginPage.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -11,7 +12,15 @@ namespace EjemploCursoWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            WSHttpBinding basicHttp = new WSHttpBinding();
+            basicHttp.Security.Mode = SecurityMode.None;
+            basicHttp.Name = "MetadataExchangeHttpBinding_IService1";
+            
+            EndpointAddress endpoint = new EndpointAddress("http://localhost:56830/Service1.svc/mex");
 
+            ServiceReference1.Service1Client obj = new ServiceReference1.Service1Client(basicHttp, endpoint);
+
+            var datos = obj.ObtenerUsuarios();
         }
     }
 }
