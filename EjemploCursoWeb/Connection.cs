f26bd4c87028d490140a4337ab5dd869eba61ef6
agil <no-reply@no-email.com>
2024-07-10 14:46:49 +0200

    Movemos la conexión a una nueva clase

---
+++ EjemploCursoWeb/Connection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace EjemploCursoWeb
+{
+    public class Connection
+    {
+        public ServiceReference1.Service1Client NewConnection()
+        {
+            WSHttpBinding basicHttp = new WSHttpBinding();
+            basicHttp.Security.Mode = SecurityMode.None;
+            basicHttp.Name = "MetadataExchangeHttpBinding_IService1";
+
+            EndpointAddress endpoint = new EndpointAddress("http://localhost:56830/Service1.svc/mex");
+
+            return new ServiceReference1.Service1Client(basicHttp, endpoint);
+        }
+    }
+}
--- EjemploCursoWeb/LoginPage.aspx.cs
+++ EjemploCursoWeb/LoginPage.aspx.cs
@@ -12,15 +12,7 @@ namespace EjemploCursoWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            WSHttpBinding basicHttp = new WSHttpBinding();
-            basicHttp.Security.Mode = SecurityMode.None;
-            basicHttp.Name = "MetadataExchangeHttpBinding_IService1";
             
-            EndpointAddress endpoint = new EndpointAddress("http://localhost:56830/Service1.svc/mex");
-
-            ServiceReference1.Service1Client obj = new ServiceReference1.Service1Client(basicHttp, endpoint);
-
-            var datos = obj.ObtenerUsuarios();
         }
     }
 }
